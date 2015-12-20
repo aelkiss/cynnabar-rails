6e35512a7028b1fe8137137c3c7fc73228ea19b3
Aaron Elkiss <aelkiss@gmail.com>
2015-12-20 08:45:18 -0500

    add unique index for slugs on pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219160127) do
+ActiveRecord::Schema.define(version: 20151220133339) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "body"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20151219160127) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "pages", ["slug"], name: "index_pages_on_slug", unique: true
 
 end
