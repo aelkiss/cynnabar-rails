6f57e2b8600394e87fac8e91bac304ff3d444ace
Aaron Elkiss <aelkiss@gmail.com>
2015-12-29 11:37:43 -0500

    fix test for officer email links

--- spec/views/offices/index.html.erb_spec.rb
+++ spec/views/offices/index.html.erb_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe "offices/index", type: :view do
     offices.each do |office|
       assert_select ".office_listed[id=office#{office.id}]", :count => 1 do
         assert_select ".office_name", :text => office.name, :count => 1
-        assert_select ".officer_name>a[href='#{office.email}']", :count => 1
+        # obfuscated email link
+        assert_select ".officer_name>script", :count => 1
         assert_select "img[src='#{office.image}']", :count => 1
       end
     end
