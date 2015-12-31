5cb4724100aa25a079b30ce91786af07221c30d1
Aaron Elkiss <aelkiss@gmail.com>
2015-12-30 20:36:16 -0500

    make cynnabar header image a link

--- db/migrate/20151224010417_create_offices.rb
+++ db/migrate/20151224010417_create_offices.rb
@@ -5,7 +5,7 @@ class CreateOffices < ActiveRecord::Migration
       t.string :email
       t.string :image
       t.references :page, index: true, foreign_key: true
-      t.references :officer, index: true, foreign_key: true
+      t.references :officer, references: :users, index: true, foreign_key: true
 
       t.timestamps null: false
     end
