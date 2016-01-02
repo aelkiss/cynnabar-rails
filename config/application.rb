a3626f2d6f7ce2c3fb3d87450fac0d8d8308cd9b
Aaron Elkiss <aelkiss@gmail.com>
2016-01-01 21:46:04 -0500

    allow local console

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module CynnabarRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.web_console.whitelisted_ips = '192.168.0.0/16'
   end
 end
