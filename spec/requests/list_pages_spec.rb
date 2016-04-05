dded3359a0ea054368d6e0bddb4165a0871c3c43
Aaron Elkiss <aelkiss@gmail.com>
2016-04-04 22:08:56 -0400

    List page owner in page list
     - add css formatting for page list
     - organize/add comments in css

--- spec/requests/list_pages_spec.rb
+++ spec/requests/list_pages_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 require 'pry'
 
 describe "GET /pages" do
-  it "as an admin, shows all the pages with title and slug" do
+  it "as an admin, shows all the pages with title, slug, and owner" do
     sign_in(create(:user, :admin))
+    user = create(:user)
     page1 = create(:page)
+    page1.user = user
     page2 = create(:page)
 
     get pages_path
@@ -13,6 +15,7 @@ describe "GET /pages" do
     expect(response.body).to include("<a href=\"/#{page1.slug}\">/#{page1.slug}</a>")
     expect(response.body).to include(page2.title)
     expect(response.body).to include("<a href=\"/#{page2.slug}\">/#{page2.slug}</a>")
+    expect(response.body).to include(user.name)
   end
 
   it "as a normal user, does not list pages" do
