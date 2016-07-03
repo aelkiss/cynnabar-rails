eccb9832694ea53bc8f73d702f94c262c2b884bb
Aaron Elkiss <aelkiss@gmail.com>
2016-07-03 09:26:19 -0400

    feature spec: edit linked recipient bio

--- spec/features/edit_user_recipient_spec.rb
+++ spec/features/edit_user_recipient_spec.rb
@@ -7,13 +7,15 @@ RSpec.feature 'Edit linked recipient' do
   include_context 'when signed in through capybara'
 
   scenario 'persists changes to the recipient' do
-    user = create(:user, :has_recipient)
+    recipient = create(:recipient, sca_name: 'Robert Exaumplemanne')
+    user = create(:user, recipient: recipient)
     sign_in(user)
 
     click_link('Edit Profile', match: :first)
 
     fill_in 'recipient_title', with: 'Grand Poobah'
     fill_in 'recipient_pronouns', with: 'Its'
+    fill_in 'recipient_sca_bio', with: 'Sample bio'
     click_on 'Update Profile'
 
     expect(page).not_to have_content('Group?')
@@ -24,5 +26,7 @@ RSpec.feature 'Edit linked recipient' do
     expect(recipient.pronouns).to eq('Its')
 
     expect(page).to have_content('Profile was successfully updated')
+    expect(page).to have_content('Sample bio')
+    expect(page).to have_content("Grand Poobah #{user.recipient.sca_name}")
   end
 end
