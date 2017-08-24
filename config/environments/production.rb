9278cf001ff6a70eb9ec7d26b09f3832a1dd55bb
Aaron Elkiss <aelkiss@gmail.com>
2017-08-24 12:22:05 -0400

    add actionmailer default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.default_url_options = { :host => "www.cynnabar.org" }
 end
