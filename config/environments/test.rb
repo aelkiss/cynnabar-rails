8d7f62a4b383d38daeae9c48644f25ad963ef56c
Aaron Elkiss <aelkiss@gmail.com>
2017-10-31 21:57:53 -0400

    set default activejob queue adapter inline for testing
     fixes failing mailer feature tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Run jobs inline for testing purposes (needed for mailer feature tests)
+  config.active_job.queue_adapter = :inline
 end
