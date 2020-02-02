5f0b09b200856679694a6276f91a3ade33a326ec
Aaron Elkiss <aelkiss@gmail.com>
2020-02-02 12:32:57 -0500

    Custom logos on pages

--- spec/views/pages/edit.html.erb_spec.rb
+++ spec/views/pages/edit.html.erb_spec.rb
@@ -2,12 +2,20 @@
 require 'rails_helper'
 
 RSpec.describe 'pages/edit', type: :view do
-  it 'has an option for selecting the owner' do
+  before(:each) do
     @page = assign(:page, create(:page))
     admin = build(:user, :admin)
     allow(controller).to receive(:current_ability).and_return(Ability.new(admin))
+  end
+
+  it 'has an option for selecting the owner' do
     user = create(:user)
     render
     assert_select 'option', text: user.to_s
+  end
+
+  it 'has an option for adding a custom logo' do
+    render
+    assert_select "input[name='page[logo]']"
   end
 end
